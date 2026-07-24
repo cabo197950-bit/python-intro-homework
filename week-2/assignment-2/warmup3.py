afaeb4934c9fcec3fe346a13002a7ded91e9c21a
Cristina Lopes Ayres <cabo197950@gmail.com>
2026-07-24 13:15:26 -0400

    Complete week 2 assignment

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -1,3 +1,6 @@
+# git log --oneline output:
+# c3d3c71 Add warmup1 and warmup2 for week 2
+
 # This week I learned how to use the terminal,
 # run Python scripts, and work with Git and GitHub.
 
---
+++ week-2/assignment-2/warmup4.py
@@ -0,0 +1,15 @@
+# Error message:
+# NameError: name 'age' is not defined
+#
+# Cause:
+# I tried to use a variable that was never created.
+#
+# Fix:
+# I created the variable before using it.
+
+age = 45
+
+print(age) 
+
+
+
